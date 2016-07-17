ba89d32d5ae376530e42d9329d5b80bdd2dc1ef1
DanielDaddato <daniel.daddato@gmail.com>
2016-07-17 17:48:36 -0300

    subindo arquivos para armazenamentos

---
+++ SQLQuery1.sql
@@ -0,0 +1,20 @@
+﻿USE [TestePratico]
+GO
+
+/****** Object: Table [dbo].[Categoria] Script Date: 17/07/2016 16:50:28 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Categoria] (
+    [Id]        INT        Primary key    IDENTITY (1, 1) NOT NULL,
+    [Nome]      NVARCHAR (100) NOT NULL,
+    [Descricao] NVARCHAR (150) NULL
+);
+
+ALTER TABLE [dbo].[Produtos]
+    ADD CONSTRAINT [FK_Produtos_Categoria] FOREIGN KEY ([CategoriaId]) REFERENCES [dbo].[Categoria] ([Id]);
+
+
---
+++ SQLQuery2.sql
@@ -0,0 +1,24 @@
+﻿USE [TestePratico]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+DROP TABLE [dbo].[Produtos];
+
+
+GO
+CREATE TABLE [dbo].[Produtos] (
+    [Id]          INT        PRIMARY KEY     IDENTITY (1, 1) NOT NULL,
+    [Nome]        NVARCHAR (100)  NOT NULL,
+    [Descricao]   NVARCHAR (150)  NULL,
+    [Preco]       DECIMAL (18, 2) NOT NULL,
+    [Quantidade]  INT             NOT NULL,
+    [CategoriaId] INT             NULL
+);
+
+ALTER TABLE [dbo].[Produtos]
+    ADD CONSTRAINT [FK_Produtos_Categoria] FOREIGN KEY ([CategoriaId]) REFERENCES [dbo].[Categoria] ([Id]);
